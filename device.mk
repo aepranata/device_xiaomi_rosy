32cefc8d95899d64f4cb4f0d149ae09057ada330
Alexander Martinz <amartinz@shiftphones.com>
2022-10-17 18:16:15 +0200

    Revert "rosy: Disable APEX compression"
    This reverts commit 251f880b733abf9dfbbd9cf3d8b6f214f09a65b9.
    
    Compressed APEX support is fixed kernel side.
    
    Test: build, flash and boot device with flag reverted.
    Test: boot GSI with compressed APEX enabled.
    
    Change-Id: I5988614cc055d565b17a5c8d830e35feb0e33e92
    Signed-off-by: Alexander Martinz <amartinz@shiftphones.com>
    Signed-off-by: Aditya Eka Pranata <aepranata@gmail.com>

--- device.mk
+++ device.mk
@@ -28,9 +28,6 @@ PRODUCT_DEX_PREOPT_BOOT_IMAGE_PROFILE_LOCATION := frameworks/base/config/boot-im
 # Speed profile services and wifi-service to reduce RAM and storage
 PRODUCT_SYSTEM_SERVER_COMPILER_FILTER := speed-profile
 
-# APEX
-PRODUCT_COMPRESSED_APEX := false
-
 # Overlays
 DEVICE_PACKAGE_OVERLAYS += $(LOCAL_PATH)/overlay
 DEVICE_PACKAGE_OVERLAYS += $(LOCAL_PATH)/overlay-lineage
