0270f05b9baf1f5521dd0d36db7801d13c3abdaa
Aditya Eka Pranata <aepranata@gmail.com>
2022-10-22 19:56:42 +0700

    rosy: Convert to our device tree
    Signed-off-by: Aditya Eka Pranata <aepranata@gmail.com>

--- aosp_rosy.mk
+++ aosp_rosy.mk
@@ -4,10 +4,8 @@
 # SPDX-License-Identifier: Apache-2.0
 #
 
-# Inherit from those products. Most specific first.
-$(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
-$(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
-$(call inherit-product, $(SRC_TARGET_DIR)/product/product_launched_with_n_mr1.mk)
+# Inherit from rosy device
+$(call inherit-product, device/xiaomi/rosy/full_rosy.mk)
 
 # Inherit some common PixelExperience stuff
 $(call inherit-product, vendor/aosp/config/common_full_phone.mk)
@@ -17,21 +15,5 @@ TARGET_SUPPORTS_GOOGLE_RECORDER := false
 TARGET_INCLUDE_STOCK_ARCORE := false
 TARGET_INCLUDE_LIVE_WALLPAPERS := false
 
-# Inherit from rosy device
-$(call inherit-product, device/xiaomi/rosy/device.mk)
-
 # Device identifier. This must come after all inclusions
 PRODUCT_NAME := aosp_rosy
-PRODUCT_DEVICE := rosy
-PRODUCT_BRAND := Xiaomi
-PRODUCT_MODEL := Redmi 5
-PRODUCT_MANUFACTURER := Xiaomi
-
-# Boot animation
-TARGET_BOOT_ANIMATION_RES := 720
-TARGET_BOOTANIMATION_HALF_RES := true
-
-# GApps
-TARGET_GAPPS_ARCH := arm64
-
-PRODUCT_GMS_CLIENTID_BASE := android-xiaomi
---
+++ full_rosy.mk
@@ -0,0 +1,28 @@
+#
+# Copyright (C) The LineageOS Project
+#
+# SPDX-License-Identifier: Apache-2.0
+#
+
+# Inherit from those products. Most specific first.
+$(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
+$(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
+$(call inherit-product, $(SRC_TARGET_DIR)/product/product_launched_with_n_mr1.mk)
+
+# Inherit from rosy device
+$(call inherit-product, device/xiaomi/rosy/device.mk)
+
+# Device identifier. This must come after all inclusions
+PRODUCT_DEVICE := rosy
+PRODUCT_BRAND := Xiaomi
+PRODUCT_MODEL := Redmi 5
+PRODUCT_MANUFACTURER := Xiaomi
+
+# Boot animation
+TARGET_BOOT_ANIMATION_RES := 720
+TARGET_BOOTANIMATION_HALF_RES := true
+
+# GApps
+TARGET_GAPPS_ARCH := arm64
+
+PRODUCT_GMS_CLIENTID_BASE := android-xiaomi
