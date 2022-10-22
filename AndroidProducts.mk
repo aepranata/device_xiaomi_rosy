3f1200c52614715a6e7d582cbcd6b98718f861f8
Aditya Eka Pranata <aepranata@gmail.com>
2022-10-22 20:02:39 +0700

    rosy: Add BananaDroid support
    Signed-off-by: Aditya Eka Pranata <aepranata@gmail.com>

--- AndroidProducts.mk
+++ AndroidProducts.mk
@@ -11,3 +11,11 @@ COMMON_LUNCH_CHOICES := \
     aosp_rosy-user \
     aosp_rosy-userdebug \
     aosp_rosy-eng
+
+PRODUCT_MAKEFILES += \
+    $(LOCAL_DIR)/banana_rosy.mk
+
+COMMON_LUNCH_CHOICES += \
+    banana_rosy-user \
+    banana_rosy-userdebug \
+    banana_rosy-eng
---
+++ banana_rosy.mk
@@ -0,0 +1,18 @@
+#
+# Copyright (C) The LineageOS Project
+#
+# SPDX-License-Identifier: Apache-2.0
+#
+
+# Inherit from rosy device
+$(call inherit-product, device/xiaomi/rosy/full_rosy.mk)
+
+# Inherit some common BananaDroid stuff
+$(call inherit-product, vendor/banana/config/common.mk)
+
+# BananaDroid Features
+BANANA_MAINTAINER := aepranata
+TARGET_FACE_UNLOCK_SUPPORTED := true
+
+# Device identifier. This must come after all inclusions
+PRODUCT_NAME := banana_rosy
