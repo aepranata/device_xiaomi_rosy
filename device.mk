e94f1e0d783f2d0487ca9b04a73f8f0699618c71
Aditya Eka Pranata <aepranata@gmail.com>
2025-12-08 20:27:39 +0700

    rosy: Drop non-existent modules
    * android.hardware.audio.sounddose-vendor-impl
    
    Change-Id: Ie9c09b0f857d27863f1a29a6adcc8f0a27a63011

--- device.mk
+++ device.mk
@@ -94,7 +94,6 @@ PRODUCT_PACKAGES += \
     android.hardware.audio.service \
     android.hardware.audio@7.1-impl:32 \
     android.hardware.audio.effect@7.0-impl:32 \
-    android.hardware.audio.sounddose-vendor-impl \
     android.hardware.soundtrigger@2.1-impl:32 \
     android.hardware.bluetooth.audio-impl:32 \
     audio.bluetooth.default \
